4050b0d886d465182c98d5c95c95144a6133028b
Omar Mohamed <omar01028147534@gmail.com>
2023-03-20 02:34:06 +0200

    Fix: handling multiple roles changes

--- src/functions/roleWatcher.js
+++ src/functions/roleWatcher.js
@@ -5,47 +5,51 @@ const roleWatcher = async (activeMember, oldState, newState) => {
   let oldDifference = oldState.roles.cache.map(i => i.id);
   let newDifference = newState.roles.cache.map(i => i.id);
 
-  const changedRole = difference(oldDifference, newDifference).length
-    ? activeMember.guild.roles.cache.get(difference(oldDifference, newDifference)[0])
-    : activeMember.guild.roles.cache.get(difference(newDifference, oldDifference)[0]);
-
   if (difference(oldDifference, newDifference).length > 0 || difference(newDifference, oldDifference).length > 0) {
     const roleSpreadersIndexes = GlobalVars.roleSpreaders.map(roleId => activeMember?.guild.roles.cache.get(roleId));
 
-    if (difference(oldDifference, newDifference).length) {
-      for (let i = 0; i < roleSpreadersIndexes.length; i++) {
-        if (
-          roleSpreadersIndexes[i]?.rawPosition < changedRole?.rawPosition &&
-          changedRole?.rawPosition <= roleSpreadersIndexes[i - 1]?.rawPosition
-        ) {
+    const rolesAdded = difference(oldDifference, newDifference);
+    if (rolesAdded.length) {
+      rolesAdded.forEach(async changedRole => {
+        const addedRole = activeMember.guild.roles.cache.get(changedRole);
+
+        for (let i = 0; i < roleSpreadersIndexes.length; i++) {
           if (
-            (await activeMember.roles.cache
-              .map(i => i)
-              .filter(
-                role =>
-                  roleSpreadersIndexes[i]?.rawPosition < role?.rawPosition &&
-                  role?.rawPosition < roleSpreadersIndexes[i - 1]?.rawPosition
-              ).length) >= 2
-          )
-            return;
+            roleSpreadersIndexes[i]?.rawPosition < addedRole?.rawPosition &&
+            addedRole?.rawPosition <= roleSpreadersIndexes[i - 1]?.rawPosition
+          ) {
+            if (
+              (await activeMember.roles.cache
+                .map(i => i)
+                .filter(
+                  role =>
+                    roleSpreadersIndexes[i]?.rawPosition < role?.rawPosition &&
+                    role?.rawPosition < roleSpreadersIndexes[i - 1]?.rawPosition
+                ).length) >= 2
+            )
+              break;
 
-          activeMember.roles.remove(roleSpreadersIndexes[i - 1]);
+            activeMember.roles.remove(roleSpreadersIndexes[i - 1]);
+          }
         }
-      }
+      });
     }
 
-    if (difference(newDifference, oldDifference).length) {
-      for (let i = 0; i < roleSpreadersIndexes.length; i++) {
-        if (
-          roleSpreadersIndexes[i]?.rawPosition < changedRole?.rawPosition &&
-          changedRole?.rawPosition <= roleSpreadersIndexes[i - 1]?.rawPosition
-        ) {
-          activeMember.roles.add(roleSpreadersIndexes[i - 1]);
-        }
-      }
-    }
+    const rolesRemoved = difference(newDifference, oldDifference);
+    if (rolesRemoved.length) {
+      rolesRemoved.forEach(async changedRole => {
+        const removedRole = activeMember.guild.roles.cache.get(changedRole);
 
-    activeMember.roles.cache.filter(role => role.rawPosition);
+        for (let i = 0; i < roleSpreadersIndexes.length; i++) {
+          if (
+            roleSpreadersIndexes[i]?.rawPosition < removedRole?.rawPosition &&
+            removedRole?.rawPosition <= roleSpreadersIndexes[i - 1]?.rawPosition
+          ) {
+            activeMember.roles.add(roleSpreadersIndexes[i - 1]);
+          }
+        }
+      });
+    }
   }
 };
 
